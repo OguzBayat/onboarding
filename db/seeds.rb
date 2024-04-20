0cd910319662b23cd8e0d8c59a6c066a91681276
oguz.bayat <oguz.bayat@sapiens.com>
2024-04-21 00:26:06 +0300

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.create(email: 'oguzbayat77@gmail.com', name: 'Oguz', surname: 'Bayat')
+company = Company.create(title: 'test comp', onboarding_completed: false, user_id: user.id)
+
+step_titles = [
+  'Welcome to Fabrikator',
+  'Set your order lead time',
+  'Set days of stock for products',
+  'Set forcasting days',
+  'Upload existing POs',
+  'Match suppliers and products',
+  'Set bundles',
+  'Set integrations'
+]
+
+step_titles.each_with_index do |title, index|
+  company.onboarding_steps.create(
+    title: title,
+    description: Faker::Lorem.paragraph(sentence_count: 5),
+    video_info: Faker::Lorem.sentence(word_count: 3),
+    step_order: index + 1,
+    locked: false
+  )
+end
