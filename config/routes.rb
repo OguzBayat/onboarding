6cd505d9ea400173e84bda4ae6308ba32503ccca
Oğuz Bayat <oguzbayat77@gmail.com>
2024-04-21 18:43:20 +0300

    Add onboarding controller routes and rack-cors gem (#9)
    Co-authored-by: oguz.bayat <oguz.bayat@sapiens.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  # API routes hould be in /api/v1
+  namespace :api do
+    namespace :v1 do
+      get ':company_id/onboarding_steps', to: 'onboarding_steps#index'
+      get ':company_id/onboarding_steps/:id', to: 'onboarding_steps#show'
+      put ':company_id/onboarding_steps/:id', to: 'onboarding_steps#update'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
