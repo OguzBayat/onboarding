38a9ef6aca874bb5031e3bdde393e005a2f9d550
Oğuz Bayat <oguzbayat77@gmail.com>
2024-04-22 02:45:51 +0300

    Create sync_progress_infos controller and endpoint (#16)
    Co-authored-by: oguz.bayat <oguz.bayat@sapiens.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # API routes hould be in /api/v1
   namespace :api do
     namespace :v1 do
+      get ':company_id/sync_progress_infos', to: 'sync_progress_infos#index'
+
       get ':company_id/onboarding_steps', to: 'onboarding_steps#index'
       get ':company_id/onboarding_steps/:id', to: 'onboarding_steps#show'
       put ':company_id/onboarding_steps/:id', to: 'onboarding_steps#update'
