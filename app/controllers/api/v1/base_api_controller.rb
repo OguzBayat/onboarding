3b9701f3b1ab1f86468288501a644161de2ab908
oguz.bayat <oguz.bayat@sapiens.com>
2024-04-23 21:48:50 +0300

    Add Iam endpoint for get user and company data

--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::BaseApiController < ActionController::API
+  def me
+    render json: current_company, serializer: Api::V1::IamSerializer
+  end
+
   def current_company
     @current_company = Company.find(params[:company_id])
   end
